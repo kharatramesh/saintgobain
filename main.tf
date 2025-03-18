782ad6a89b4e55d4e225fcd4b6c53dd605c175f1
trainer <trainer@gmail.com>
2025-03-18 12:53:14 +0530

    6

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ resource "azurerm_virtual_network" "vnet1" {
   address_space = ["21.21.21.0/24"]
   resource_group_name = azurerm_resource_group.rg1.name
 }
+
+resource "azurerm_virtual_network" "vnet2" {
+  name     = "trainer-saintgobain-vnet2"
+  location = var.region
+  address_space = ["22.21.21.0/24"]
+  resource_group_name = azurerm_resource_group.rg1.name
+}
